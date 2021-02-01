6e1c92f59cb523620475b92b88a0b4c98e4784e5
Joseph Babaki <59181471+josebbk@users.noreply.github.com>
2021-02-01 22:15:41 +0330

    Secret Key Removed

--- MyEcommerce/settings.py
+++ MyEcommerce/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r4**%zsh4yo=3z@#g_-px43e^7#)f-_!^ktcl@2=(bf!jox$i5'
-# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
